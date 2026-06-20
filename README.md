f66c92629c67a6aa8565b036a48260e1b06080e1
Hassan Ezzeldeen <dhassanezz98@gmail.com>
2026-06-20 22:19:30 +0300

    add readme + env file

---
+++ README.md
@@ -0,0 +1,5 @@
+# Maze runners
+
+2D Online mulitplayer game dev expirement.
+
+![Screenshot](./docs/img/ss.png)
--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hasssanezzz/mazerunners/pkg/client"
 	"github.com/hasssanezzz/mazerunners/pkg/config"
 )
 
-func main() {
+func runClient() {
+	os.Getenv("MZRN")
+
+	// playerName := os.Getenv("MZRN_PLAYER_NAME")
+	// playerPassword := os.Getenv("MZRN_PLAYER_PASSWORD")
+	// serverAddress := os.Getenv("MZRN_SERVER_ADDRESS")
+
 	cfg := &config.Config{
-		WindowSize: 500,
+		WindowSize: 800,
 		CellSize:   25,
-		MapSize:    100,
+		MapSize:    50,
 	}
 
 	ebiten.SetWindowSize(cfg.WindowSize, cfg.WindowSize)
@@ -21,4 +28,12 @@ func main() {
 	if err := ebiten.RunGame(client.NewGame(cfg)); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func main() {
+	if len(os.Args) > 1 && os.Args[1] == "serve" {
+		return
+	}
+
+	runClient()
 }
